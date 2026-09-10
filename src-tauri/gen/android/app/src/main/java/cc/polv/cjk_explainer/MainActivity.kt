63e7176c2cdbe4b8674f0bfcd78f56a8bfdc16da
polv <polv@polv.cc>
2026-09-10 23:24:38 +0700

    webview android padding

--- src-tauri/gen/android/app/src/main/java/cc/polv/cjk_explainer/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/cc/polv/cjk_explainer/MainActivity.kt
@@ -1,11 +1,44 @@
 package cc.polv.cjk_explainer
 
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+
+    val rootView: View = findViewById(android.R.id.content)
+
+    ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      val imeVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+      val imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+
+      val bottomPadding = if (imeVisible) imeHeight else systemBars.bottom
+
+      v.setPadding(
+        systemBars.left,
+        systemBars.top,
+        systemBars.right,
+        bottomPadding
+      )
+      insets
+    }
+  }
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+
+    webView.setBackgroundColor(0x00000000)
+
+    webView.fitsSystemWindows = true
+
+    android.util.Log.i("MainActivity", "WebView created, waiting for Rust hide() call...")
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@ async function greet() {
 </script>
 
 <template>
-  <SidebarLayout>
+  <SidebarLayout style="overflow: hidden">
     <SidebarBackdrop v-if="isMobile && (navOpen || open)" />
     <Sidebar
       id="nav"
@@ -190,10 +190,7 @@ async function greet() {
 <style>
 body {
   margin: 0;
-  padding-top: env(safe-area-inset-top);
-  padding-bottom: env(safe-area-inset-bottom);
-  padding-left: env(safe-area-inset-left);
-  padding-right: env(safe-area-inset-right);
+  padding: 0;
   overflow: hidden;
   height: 100vh;
   width: 100vw;
