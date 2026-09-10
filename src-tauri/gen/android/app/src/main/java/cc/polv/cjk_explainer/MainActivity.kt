169214afd204e05df9bf2ae37a5073bd9f20a616
polv <polv@polv.cc>
2026-09-10 21:22:09 +0700

    template with tauri plugins

---
+++ src-tauri/gen/android/app/src/main/java/cc/polv/cjk_explainer/MainActivity.kt
@@ -0,0 +1,11 @@
+package cc.polv.cjk_explainer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
