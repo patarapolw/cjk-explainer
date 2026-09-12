e68eeef6b76bef73557ee01ac8170ce9f0ab3cc2
polv <polv@polv.cc>
2026-09-12 08:30:27 +0700

    remove boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <SidebarLayout style="overflow: hidden">
+  <SidebarLayout class="layout">
     <SidebarBackdrop v-if="isMobile && (navOpen || open)" />
     <Sidebar
       id="nav"
@@ -91,16 +91,7 @@ onBeforeUnmount(() => {
     </Sidebar>
 
     <SidebarMain>
-      <header
-        style="
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          height: 3rem;
-          padding: 0.5rem;
-          gap: 0.5rem;
-        "
-      >
+      <header class="header">
         <SidebarTrigger
           target="nav"
           severity="secondary"
@@ -109,14 +100,31 @@ onBeforeUnmount(() => {
         >
           <SidebarIcon />
         </SidebarTrigger>
-        <span class="text-sm font-medium flex-1" style="flex-grow: 1">
-          Dashboard
-        </span>
+        <span class="header-panel"> Dashboard </span>
       </header>
       <RouterView />
     </SidebarMain>
   </SidebarLayout>
 </template>
+
+<style scoped>
+.layout {
+  overflow: hidden;
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 3rem;
+  padding: 0.5rem;
+  gap: 0.5rem;
+}
+
+.header-panel {
+  flex-grow: 1;
+}
+</style>
 
 <style>
 body {
@@ -128,83 +136,12 @@ body {
 }
 
 :root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
+  font-family: sans-serif;
 
   font-synthesis: none;
   text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   -webkit-text-size-adjust: 100%;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
 }
 </style>
